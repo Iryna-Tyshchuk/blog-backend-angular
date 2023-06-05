9f0fe531e8664c1841c35c374a4cd5a1e0d714c9
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-06-05 22:45:24 +0300

    delete comments from posts controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,60 +3,6 @@ const { Post } = require("../models/post");
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { getAllPostsService } = require("../services/postService");
 
-// const getAll = async (req, res) => {
-//   const { page, perPage, search, startDate, endDate, topic, filter } =
-//     req.query;
-
-//   const pageNumber = parseInt(page) || 1;
-//   const itemsPerPage = parseInt(perPage) || 10;
-
-//   const pipeline = [];
-
-//   const $match = {};
-
-//   if (search) {
-//     const searchRegex = new RegExp(search, "i");
-//     $match["$or"] = [
-//       { title: { $regex: searchRegex } },
-//       { subtitle: { $regex: searchRegex } },
-//       { description: { $regex: searchRegex } },
-//     ];
-//   }
-
-//   if (startDate && endDate) {
-//     $match["postDate"] = {
-//       $gte: new Date(startDate),
-//       $lte: new Date(endDate),
-//     };
-//   }
-
-//   if (topic) {
-//     $match["topic"] = topic;
-//   }
-
-//   // const postsCount = await Post.countDocuments($match);
-//   // if (postsCount) [];
-//   const totalPosts = await Post.countDocuments($match);
-//   pipeline.push({ $match });
-
-//   if (filter === "Alphabet") {
-//     pipeline.push({ $sort: { title: 1 } });
-//   } else if (filter === "Date created") {
-//     pipeline.push({ $sort: { postDate: -1 } });
-//   }
-
-//   pipeline.push(
-//     { $skip: (pageNumber - 1) * itemsPerPage },
-//     { $limit: itemsPerPage }
-//   );
-
-//   pipeline.push({ $sort: { postDate: -1 } });
-
-//   const posts = await Post.aggregate(pipeline);
-
-//   res.json({ posts, totalPosts });
-// };
-
 const getPostsByTopic = async (req, res) => {
   const { topic } = req.query;
   let query = {};
@@ -83,15 +29,6 @@ const add = async (req, res) => {
 };
 
 const updateById = async (req, res) => {
-  // const { postId } = req.params;
-  // const result = await Post.findByIdAndUpdate(postId, req.body, {
-  //   new: true,
-  // });
-  // if (!result) {
-  //   throw HttpError(404, "Not found");
-  // }
-  // res.json(result);
-
   const { postId } = req.params;
   const { title, subtitle, description, topic } = req.body;
   const result = await Post.updateOne(
@@ -133,7 +70,6 @@ const deleteById = async (req, res) => {
     message: "Delete success",
   });
 };
-
 
 const getAll = async (req, res) => {
   const { page, perPage, search, startDate, endDate, topic, filter } =
